46f0bf54ede189353c7db0bfff6435faef815ca9
mgyarmathy <mgyarmathy2011@gmail.com>
2020-04-02 12:55:32 -0500

    added init.sh

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotfiles
 dotfiles, by @mgyarmathy ™
+
+## `init.sh`
+Installs commonly used applications and packages and copies configuration files.
+
+After running this script, do the following:
+1. install the Snazzy theme for Terminal.app from tmp/Snazzy.terminal
+1. map Caps Lock key to Escape (System Preferences -> Keyboard -> Modifier Keys...)
---
+++ init.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+mkdir -p tmp
+
+# copy bash configuration
+cp .bash_profile ~/.bash_profile
+
+# install Snazzy theme
+curl -s -o tmp/Snazzy.terminal https://github.com/sindresorhus/terminal-snazzy/raw/master/Snazzy.terminal
+
+# install Homebrew
+if ! [ -x "$(command -v brew)" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# install packages
+brew install \
+  docker \
+  kubernetes-cli \
+  minikube \
+  python \
+  watch \
+
+# install applications
+brew cask install \
+  aware \
+  docker \
+  dropbox \
+  google-chrome \
+  notion \
+  sourcetree \ 
+  spectacle \
+  spotify \
+  visual-studio-code \
+
+# vscode extensions
+code \
+  --install-extension dart-code.dart-code \
+  --install-extension ms-python.python \
+  --install-extension ms-vsliveshare.vsliveshare \
+
+# additional utilities
+curl -fsSL -o tmp/kiibohd.dmg https://github.com/kiibohd/configurator/releases/download/v1.0.2/kiibohd-configurator-1.0.2-mac.dmg
+open tmp/kiibohd.dmg
+
+
