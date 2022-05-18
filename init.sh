03830b5d1904a13ce38481ad546c364e6a6fcb55
Michael Gyarmathy <mgyarmathy@pgahq.com>
2022-05-18 12:34:04 -0500

    update init.sh

--- init.sh
+++ init.sh
@@ -12,14 +12,12 @@ open tmp/Snazzy.terminal
 # install Homebrew
 if ! [ -x "$(command -v brew)" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile
+  source ~/.bash_profile
 fi
 
-# install packages
-brew install \
-  watch \
-
 # install applications
-brew cask install \
+brew install --cask \
   aware \
   docker \
   dropbox \
@@ -29,11 +27,26 @@ brew cask install \
   sourcetree \
   spectacle \
   spotify \
+  tableplus \
   visual-studio-code \
 
 # vscode extensions
 code \
   --install-extension ms-vsliveshare.vsliveshare \
+
+# git config
+echo "Configuring git..."
+read -p 'git config --global user.name: ' gitusername
+git config --global user.name $gitusername
+
+read -p 'git config --global user.email: ' gitemail
+git config --global user.email $gitemail
+
+echo 'git config --global pull.rebase true'
+git config --global pull.rebase true
+
+echo 'git config --global fetch.prune true'
+git config --global fetch.prune true
 
 # additional utilities
 curl -fsSL -o tmp/kiibohd.dmg https://github.com/kiibohd/configurator/releases/download/v1.0.2/kiibohd-configurator-1.0.2-mac.dmg
